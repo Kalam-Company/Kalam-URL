996530d1be193444eed920d29a6643eda8a49acd
Kalam Company <77258060+Kalam-Company@users.noreply.github.com>
2021-01-13 21:14:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,5 +34,5 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(683538773 1297684837)
+    Config.AUTH_USERS.add(683538773)
     app.run()
